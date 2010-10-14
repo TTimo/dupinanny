be42f77968078017db62bc9d277fbe0b18ad3344
System Administrator <root@timo-bessets-macbookpro41.local>
2010-10-13 21:57:05 -0500

    oops that was not working at all on Mac

--- lock.py
+++ lock.py
@@ -22,7 +22,7 @@
 # NOTE: the lock has to be released and does not expire
 # when the object goes out of scope it will try to release
 
-import sys, os, pickle, portalocker, datetime, time
+import sys, os, pickle, portalocker, datetime, time, platform, subprocess
 
 class lock:
     def __init__( self, lockfile, lockinfo, debug = True ):
@@ -32,7 +32,17 @@ class lock:
         self.debug = debug
 
     def checkProcessExists( self, pid ):
-        return os.path.exists( '/proc/%d' % pid )
+        if ( platform.system() == 'Linux' ):
+            return os.path.exists( '/proc/%d' % pid )
+        if ( platform.system() == 'Darwin' ):
+            # surely there's a better way..
+            try:
+                subprocess.check_call( [ 'ps', '-p', '%d' % pid ], stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+                # if this succeeded, then there is a process
+                return True
+            except:
+                return False
+        raise Exception( 'Need support for checking for process existence on platform %s' % platform.system() )
 
     def checkValidLock( self ):
         if ( not os.path.exists( self.lockfile ) ):
@@ -74,21 +84,21 @@ class lock:
         waitTotal = 0
         while ( self.checkValidLock() ):
             if ( wait is None ):
-                raise 'lock is busy'
+                raise Exception( 'lock is busy' )
             else:
                 if ( wait != 0 ):
                     waitTotal += waitInterval
                     if ( self.debug ):
-                        print 'waitTotal: %d wait: %d waitInterval: %d' % ( waitTotal, wait, waitInterval )
+                        print( 'waitTotal: %d wait: %d waitInterval: %d' % ( waitTotal, wait, waitInterval ) )
                     if ( waitTotal > wait ):
-                        raise 'exceeded max wait time on the lock'
+                        raise Exception( 'exceeded max wait time on the lock' )
                     time.sleep( waitInterval )
 
         # don't want blocking on acquired locks - even with the loop, there is still a possibility of stolen lock and exception here
         self.handle = file( self.lockfile, 'w' )
         portalocker.lock( self.handle, portalocker.LOCK_EX | portalocker.LOCK_NB )
         if ( self.debug ):
-            print 'acquired lock %s' % self.lockfile
+            print( 'acquired lock %s' % self.lockfile )
         pickle.dump( os.getpid(), self.handle )
         if ( expire is None ):
             expire_time = None
