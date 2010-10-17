a859625eb5233a3c18d8d22f4352efa012df70a5
System Administrator <root@timo-bessets-macbookpro41.local>
2010-10-17 08:33:31 -0500

    meh

--- backup.py
+++ backup.py
@@ -18,6 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##########################################################################
 
+from __future__ import with_statement
 from contextlib import contextmanager
 
 import sys, commands, os, subprocess, pickle, datetime
